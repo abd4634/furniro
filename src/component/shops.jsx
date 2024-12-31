a0d1f10a69ee2390433e3d625c7937dd4d1319be
Abdullah <abd4634abd@gmail.com>
2024-12-31 12:40:52 +0530

    shops.jsx

--- src/component/shops.jsx
+++ src/component/shops.jsx
@@ -8,56 +8,7 @@ import { IoFilterSharp } from "react-icons/io5";
 const shops = () => {
   return (
     <div>
-        <div
-                className="relative bg-cover bg-center bg-no-repeat h-64"
-                style={{ backgroundImage: `url(${Banner})` }}
-              >
-                <div className="absolute inset-0 bg-black/30"></div>
-                <div className="relative flex flex-col items-center justify-center h-full text-white text-center">
-                  <img src={favicon} alt="Logo" />
-                  <h1
-                    className="text-4xl font-bold"
-                    style={{
-                      fontWeight: 500,
-                      fontSize: '48px',
-                      lineHeight: '72px',
-                      color: '#000000',
-                    }}
-                  >
-                    Shops
-                  </h1>
-                  <nav className="mt-2 text-sm text-gray-300">
-                    <Link to='/'
-                      
-                      className="hover:text-white"
-                      style={{
-                        fontWeight: 400,
-                        fontSize: '16px',
-                        lineHeight: '24px',
-                        color: '#000000',
-                      }}
-                    >
-                      Home
-                    </Link>
-                    <span
-                      className="mx-2"
-                      style={{ color: '#000000' }}
-                    >
-                      &gt;
-                    </span>
-                    <span
-                      style={{
-                        fontWeight: 300,
-                        fontSize: '16px',
-                        lineHeight: '24px',
-                        color: '#000000',
-                      }}
-                    >
-                      Shop
-                    </span>
-                  </nav>
-                </div>
-              </div>
+        
               <div className="flex flex-wrap items-center justify-between bg-beige p-4" style={{ backgroundColor: '#F9F1E7',
                 fontWeight: 500, fontSize: '20px', lineHeight: '30px', color: '#000000'
                }}>
