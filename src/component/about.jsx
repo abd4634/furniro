8438ae1a30f2423f3cd7bbf3154a5f891c52eb97
Abdullah <abd4634abd@gmail.com>
2024-12-31 12:34:09 +0530

    Update About.jsx

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -6,56 +6,7 @@ import favicon from "../assets/img/favicon.png";
 const about = () => {
   return (
     <div>
-        <div
-          className="relative bg-cover bg-center bg-no-repeat h-64"
-          style={{ backgroundImage: `url(${Banner})` }}
-            >
-          <div className="absolute inset-0 bg-black/30"></div>
-          <div className="relative flex flex-col items-center justify-center h-full text-white text-center">
-            <img src={favicon} alt="Logo" />
-            <h1
-              className="text-4xl font-bold"
-              style={{
-                fontWeight: 500,
-                fontSize: '48px',
-                lineHeight: '72px',
-                color: '#000000',
-              }}
-            >
-              About us
-            </h1>
-            <nav className="mt-2 text-sm text-gray-300">
-              <Link to='/'
-                
-                className="hover:text-white"
-                style={{
-                  fontWeight: 400,
-                  fontSize: '16px',
-                  lineHeight: '24px',
-                  color: '#000000',
-                }}
-              >
-                Home
-              </Link>
-              <span
-                className="mx-2"
-                style={{ color: '#000000' }}
-              >
-                &gt;
-              </span>
-              <span
-                style={{
-                  fontWeight: 300,
-                  fontSize: '16px',
-                  lineHeight: '24px',
-                  color: '#000000',
-                }}
-              >
-                About us
-              </span>
-            </nav>
-          </div>
-        </div>
+        
     </div>
   )
 }
