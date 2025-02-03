5f712a54ccc41dba823fa73a2e844c223f67ad74
Abdullah <abd4634abd@gmail.com>
2025-02-03 18:21:36 +0530

    update about.jsx

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import Banner from "../assets/img/Rectangle1.png";
-import { Link } from 'react-router-dom';
-import favicon from "../assets/img/favicon.png";
+
 
 const about = () => {
   return (
